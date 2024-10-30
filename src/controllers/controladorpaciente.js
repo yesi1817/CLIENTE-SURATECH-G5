4538eb164ea62e00be878fc431d24cecb7f675c6
julieta grisales calle <julieta0410g@gmail.com>
2024-10-30 15:43:13 -0500

    se crea controlador

--- src/controllers/controladorpaciente.js
+++ src/controllers/controladorpaciente.js
@@ -3,6 +3,7 @@
 //1. Por cada input, select, textarea del formulario se crea una variable 
 
 //2. Por cada variable debo utilizar el DOM para asociar el html con js
+/*
 let nombrePaciente =document.getElementById("nombrepaciente")
 let correoPaciente =document.getElementById("correopaciente")
 let telefonoPaciente =document.getElementById("telefonopaciente")
@@ -49,4 +50,88 @@ Swal.fire({
     text: "Ya eres parte de nuestra gran familia!",
     icon: "success"
   });
+})
+
+*/
+
+//Objetivo: Recibir datos del BACK  y hacerles render (render=PINTARLOS)
+//1. Quemar o simular los datos 
+let pacientes=[
+    {
+        id:890,
+        nombres:"Emma Grisales",
+        fechaNacimiento:"2006-10-04",
+        ciudad:"Yarumal",
+        correo:"julieta@gmail.com",
+        telefono:"3202145050",
+        ips:"Angostura",
+        grupoIngresos:"A",
+        tienePoliza:true,
+        fechaAfiliacion:"2010-10-20"
+
+
+    },
+    {
+        id:891,
+        nombres:"Carlos Muñoz",
+        fechaNacimiento:"2000-07-12",
+        ciudad:"Medellín",
+        correo:"carlosmunoz@gmail.com",
+        telefono:"3123456789",
+        ips:"Santa Mónica",
+        grupoIngresos:"B",
+        tienePoliza:true,
+        fechaAfiliacion:"2015-03-15"
+    },
+    
+    {
+        id:892,
+        nombres:"Laura Fernández",
+        fechaNacimiento:"1995-03-25",
+        ciudad:"Bogotá",
+        correo:"laurafernandez@gmail.com",
+        telefono:"3004567890",
+        ips:"Salud Total",
+        grupoIngresos:"C",
+        tienePoliza:false,
+        fechaAfiliacion:"2018-06-20"
+    },
+    
+    {
+        id:893,
+        nombres:"Andrés López",
+        fechaNacimiento:"1989-11-30",
+        ciudad:"Cali",
+        correo:"andreslopez@gmail.com",
+        telefono:"3109876543",
+        ips:"Viva Salud",
+        grupoIngresos:"A",
+        tienePoliza:true,
+        fechaAfiliacion:"2012-09-12"
+    }
+    
+]
+
+//2. Crear una referencia a una etiqueta html donde vamos a renderizar
+let fila=document.getElementById("fila")
+
+//3. Se recorren los datos para obtenerlos de forma separada 
+pacientes.forEach(function(paciente){
+    console.log(paciente)
+    //4. Se crean columnas 
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //5. Se crean tarjetas 
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card", "p-5", "h-100", "shadow")
+
+    //6. Se crea una etiqueta para poner el nombre del paciente 
+    let nombre=document.createElement("h2")
+    nombre.textContent=paciente.nombres
+
+    //Paso final (Ordenando las cartas)
+    tarjeta.appendChild(nombre)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
 })
